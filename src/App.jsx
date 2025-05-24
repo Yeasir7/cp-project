7660b7bcba613929b08891f9865f45085a096e90
Yeasir7 <ibnayeasir@gmail.com>
2025-05-24 15:24:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Blogs from './pages/Blogs'
+import BookMark from './pages/BookMark'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <>
+      <Home></Home>
+      <Blogs></Blogs>
+      <BookMark></BookMark>
+    </>
+  )
+}
+
+export default App
